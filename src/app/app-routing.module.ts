06a6b990aea630416703b411a84f0a5c1e16eee4
Sonu Kumar <skbhati199@gmail.com>
2020-10-10 21:08:28 +0530

    initial project done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../http.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  products: Array<object> = [];
+  constructor(private http: HttpService) {}
+  _getProducts(): void {
+    this.http.getAllProducts().subscribe((data: any) => {
+      this.products = data.data;
+      console.log(this.products);
+    });
+  }
+  _addItemToCart( id, quantity): void {
+    let payload = {
+      productId: id,
+      quantity,
+    };
+    this.http.addToCart(payload).subscribe(() => {
+      this._getProducts();
+      alert('Product Added');
+    });
+  }
+  ngOnInit(): void {
+    this._getProducts();
+  }
+
+}
